5214ffefb2ad229feef0d342f64a716e0d596563
Dhruv Singh <dhruvsingh510@gmail.com>
2020-11-01 14:28:31 +0530

    Updates

---
+++ precache-manifest.61c8b14fa034568c9dfbd841e1d6a7da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03504792cbe98afd60128e24befd6ab2",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "8578dc67327a9c94a4b0",
+    "url": "/smart-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "281cb8eab9fd9e254dc8",
+    "url": "/smart-brain/static/css/main.b562dcc5.chunk.css"
+  },
+  {
+    "revision": "8578dc67327a9c94a4b0",
+    "url": "/smart-brain/static/js/2.14a8788c.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smart-brain/static/js/2.14a8788c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "281cb8eab9fd9e254dc8",
+    "url": "/smart-brain/static/js/main.dd1ae29e.chunk.js"
+  },
+  {
+    "revision": "257db6b14da30691b4a8",
+    "url": "/smart-brain/static/js/runtime-main.1bac4abf.js"
+  }
+]);
